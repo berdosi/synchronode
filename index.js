b5af18aa9ee8f45063689a8c31cb96a03cb0dffb
Balint Erdosi <erdosib@gmail.com>
2017-12-11 00:11:36 +0100

    code cleanup

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
 "use strict";
-const Logger = require("./util/logging.js");
+const logger = require("./util/logging");
 const config = require("./config.js");
-const store = {};
 
-// to implement : attempt to reload store from persitent memory
+const state = {};
+
+// to implement : attempt to reload state from persitent memory
 
 const express = require("express");
 let expressWs = require("express-ws");
 expressWs = expressWs(express());
 const app = expressWs.app;
 
-app.use(function (req, res, next) {
-  Logger.log("middleware");
-  req.testing = "testing";
-  return next();
-});
-
 app.use(express.static("static")); // serve files from ./static 
 
-app.get("/", function (req, res, next) {
-  Logger.log("get route", req.testing);
-  res.end("oh hi");
-});
+const masterConnection = require("./connections/master.js");
+const slaveConnection = require("./connections/slaves.js");
+const clientConnection = require("./connections/clients.js");
 
-app.ws("/", function (ws, req) {
-  ws.on("message", function (msg) {
-    Logger.log(msg);
-  });
-  Logger.log("socket", req.testing);
-});
+// connect to the master, if it is configured
+if (config.master) masterConnection({ config, state, logger });
 
-// if there is a master, connect to it
-if (config.master) require("./connections/master.js")({ config, store, Logger });
-
-require("./connections/slaves.js")({ config, store, Logger });
-require("./connections/clients.js")({ config, store, Logger });
+slaveConnection({ app, config, state, logger });
+clientConnection({ app, config, state, logger });
 
 // listen for requests (as master as well as a local webserver serving the swarm's files)
 app.listen(config.port);
