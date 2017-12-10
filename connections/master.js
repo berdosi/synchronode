157805a76303abc75fe81e0b629afc1339100236
Balint Erdosi <erdosib@gmail.com>
2017-12-11 00:14:20 +0100

    connection to master : initial commit

--- connections/master.js
+++ connections/master.js
@@ -1,9 +1,44 @@
 /** @module master
- * Handle the connections with the server */
+ * Handle the connections with the server 
+ * - logging in upon start
+ *     - get hostId
+ *     - open websocket with that hostId
+ * - listen for requests and answer them */
+function connectMaster(args) {
+    "use strict";
+    const https = require("https");
+    const master = args.config.master; // URL of the master to connect to, without protocol
+    const port = args.config.port;
+    const logger = args.logger;
+    const state = args.state;
 
-function connectMaster(arguments) {
-    arguments.Logger.log("to implement: connection to master");
-    // get token and put it into the store
+    // get own token and put it into the state
+    // 
+    /** @type {RequestOptions} */
+    const options = {
+        host: master.replace(/\/.*/, ""),
+        port: port,
+        path: master.replace(/[^/]*\//, "/"),
+        method: "GET",
+        headers: {}
+    };
+
+    const req = https.request(options, (response) => {
+        logger.info("status", response.statusCode);
+        const data = "";
+        response.on("data", (incoming) => { data += incoming; })
+        response.on("end", () => {
+            // state the token received from the master
+            const responseObject = JSON.parse(data);
+            logger.info("response:", responseObject);
+            state.token = responseObject.token;
+        })
+    });
+
+    req.on('error', (e) => {
+        logger.error(
+            `Master unavailable: instance will be working as a standalone master node. Problem with request: ${e.message}`);
+    });
 }
 
 module.exports = connectMaster;
