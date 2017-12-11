170faa5b61f11cc2733665c28a32af449a4ca1fc
Balint Erdosi <erdosib@gmail.com>
2017-12-11 02:08:54 +0100

    registering to master, initial commit

--- connections/master.js
+++ connections/master.js
@@ -10,13 +10,14 @@
 module.exports = function connectMaster(args) {
     "use strict";
     const https = require("https");
+    const WebSocket = require("ws");
+
     const master = args.config.master; // URL of the master to connect to, without protocol
     const port = args.config.port;
     const logger = args.logger;
     const state = args.state;
 
-    // get own token and put it into the state
-    // 
+    // get own token and put it into the state. Will be shared with Clients. 
     /** @type {RequestOptions} */
     const options = {
         host: master.replace(/\/.*/, ""),
@@ -31,10 +32,24 @@ module.exports = function connectMaster(args) {
         const data = "";
         response.on("data", (incoming) => { data += incoming; })
         response.on("end", () => {
-            // state the token received from the master
+            // store the token received from the master
             const responseObject = JSON.parse(data);
             logger.info("response:", responseObject);
             state.token = responseObject.token;
+
+            // acknowledge the token by opening a WebSocket to the master
+            const ws = new WebSocket("wss://" + host + ":" + port);
+            state.masterSocket = ws;
+
+            ws.on("open", function open() {
+                logger.log("socket open, sending back token via WebSocket");
+                ws.send(JSON.parse({ token: responseObject.token }));
+            });
+
+            ws.on("message", function incoming(message) {
+                logger.log("message from master", message);
+            })
+
         })
     });
 
