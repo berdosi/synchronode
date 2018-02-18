8efeab2303e75849e83e3d0e216ee9708705b842
Balint Erdosi <erdosib@gmail.com>
2018-02-18 18:41:17 +0100

    ui enhancements

--- static/js/browser.js
+++ static/js/browser.js
@@ -2,17 +2,43 @@
 
 /* to polyfill
 - fetch
+- backtick strings
 */
 (function() {
     "use strict";
+    /** @typedef EntryRow
+     * @prop {HTMLElement} iconElement
+     * @prop {TextNode} itemDateText
+     * @prop {TextNode} itemSizeText
+     * @prop {HTMLTableRowElement} row
+     */
+    /** @typedef StatDataResponse
+     * @prop {number} mode File type and permissions as bitmask, see http://man7.org/linux/man-pages/man7/inode.7.html
+     * @prop {number} size Size in bytes.
+     * @prop {string} atime Access time (date string)
+     * @prop {number} atimeMs Access time (ms since Epoch)
+     * @prop {string} birthtime Change time (date string)
+     * @prop {number} birthtimeMs Change time (ms since Epoch)
+     * @prop {string} mtime Modification time (date string)
+     * @prop {number} mtimeMs Modification time (ms since Epoch)
+     */
+    /** @typedef StatResponse
+     * @prop {string} filePath
+     * @prop {StatDataResponse} response
+     * @prop {string} action
+     * @prop {string} path
+     * @prop {string} requestId
+     */
     const state = {
         connectionId: "",
         dom: {
             dirListing: document.getElementById("dirListing"),
+            /** @type {Map<string,EntryRow>} */
             dirListingMap: new Map(),
         },
         slaveId: "",
         socket: new WebSocket(`wss://${location.host}/browserWs/`),
+        /** @type {Map<string,StatResponse>} */
         statDataCache: new Map(),
     };
 
@@ -45,6 +71,17 @@
             const message = JSON.parse(event.data);
             if (message.connectionId) {
                 state.connectionId = message.connectionId;
+
+                // keep the connection alive
+                setInterval(
+                    function keepAlive() {
+                        state.socket.send(
+                            JSON.stringify({
+                                "action": "keep-alive",
+                                "connectionId": state.connectionId,
+                            }));
+                        },
+                    60000);
             } else {
                 if (message.action === "stat") {
                     state.statDataCache.set(message.filePath.replace(/.*\//, ""), message);
@@ -53,6 +90,26 @@
                 }
             }
         } catch (e) { userFeedback("couldn't parse message from server"); }
+    });
+
+    Array.prototype.slice.call( document.querySelectorAll("th")).forEach(function addTableHeaderEventHandler(th) {
+        th.addEventListener("click", function tableHeaderClick() {
+            const sortType = th.dataset.sortType;
+            const fieldName = th.dataset.fieldName;
+
+            Array.prototype.slice
+                .call(state.dom.dirListing.childNodes)
+                .sort(function(one, another) {
+                    const oneValue = (sortType === "numeric")
+                        ? parseInt(one.dataset[fieldName], 10)
+                        : one.dataset[fieldName];
+                    const anotherValue = (sortType === "numeric")
+                        ? parseInt(another.dataset[fieldName], 10)
+                        : another.dataset[fieldName];
+                    return (oneValue <= anotherValue) ? -1 : 1;
+                })
+                .forEach(function(row) { state.dom.dirListing.appendChild(row); });
+        });
     });
 
     /** Fetch path from server; handle based on its type
@@ -174,7 +231,7 @@
      * This function is called by :
      * - listDir on all elements (updating the metadata for the items where it arrived before the dirListing),
      * - onMessage event handler (updating the metadata for items where it arrives after the dirListing)
-     * @param {Object} statData
+     * @param {StatResponse} statData
      * @returns {undefined}
      */
     function updateEntry(statData) {
@@ -186,14 +243,60 @@
         // convert to octal representation, and get last three digits
         const permissions = Number(statData.response.mode).toString(8).replace(/.*(...)/, "$1");
         const size = statData.response.size;
-        const modTime = new Date(statData.response.mtimeMs);
-        const modTimeString = new Date(statData.response.mtime);
+        const modTimeString = statData.response.mtime;
 
         if (!isFile) { entryRow.iconElement.className = "far fa-folder"; }
-        entryRow.itemDateText.textContent = modTimeString;
-        entryRow.itemSizeText.textContent = size;
+        entryRow.itemDateText.textContent = formatValue(statData.response.mtimeMs, "date");
+        entryRow.itemSizeText.textContent = formatValue(size, "size");
+
+        // setting data attributes on table row for sorting
+        entryRow.row.dataset.modTime = statData.response.mtimeMs;
+        entryRow.row.dataset.size = size;
+        entryRow.row.dataset.name = fileName;
     }
 
+    /** Format numeric values with pre-defined fomatters.
+     * @param {number} value The value to be formatted
+     * @param {string} mode Formatting mode. Currently "date" or "size".
+     * @returns {string} Formatted value.
+     */
+    function formatValue(value, mode) {
+        if (mode === "date") {
+            const date = new Date(value);
+            // format like: "Mar 02 17:03"
+            return ["Jan", "Feb", "Mar",
+                    "Apr", "May", "Jun",
+                    "Jul", "Aug", "Sep",
+                    "Oct", "Nov", "Dec"][date.getMonth()] +
+                   (" " + date.getDate() +
+                    " " + date.getHours() +
+                    ":" + date.getMinutes())
+                .replace(/([^0-9])([0-9])([^0-9]|$)/g, "$10$2$3");
+        }
+        if (mode === "size") {
+            // 3 significant digits + Unit (3.14kB, 314kB, 31.4MB)
+            const clip = {
+                "TB": Math.pow(10, 12),
+                // tslint:disable-next-line:object-literal-sort-keys
+                "GB": Math.pow(10, 9),
+                "MB": Math.pow(10, 6),
+                "kB": Math.pow(10, 3),
+                // tslint:disable-next-line:object-literal-sort-keys
+                "bytes": 1 };
+            const unit = ((value >= clip.TB) && "TB") ||
+                ((value >= clip.GB) && "GB") ||
+                ((value >= clip.MB) && "MB") ||
+                ((value >=  clip.kB) && "kB") ||
+                "bytes";
+            /* note: this is not rounded but clipped. But it is good enough.
+             To err is human. 0.5% is fine for machines, too.
+             It also fails at Really Huge values.
+            */
+            const numberToRound = (value / clip[unit]).toString().substr(0, 4).replace(/\.$/, "");
+
+            return numberToRound + "\u2008" + unit;
+        }
+    }
     /** Provide feedback to the user.
      * @param {any} message Message to show.
      */
