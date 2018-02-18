8c599a2d6618da6463c41bdac4e48a378037e080
Balint Erdosi <erdosib@gmail.com>
2018-02-18 02:52:49 +0100

    parse stat data

--- static/js/browser.js
+++ static/js/browser.js
@@ -7,6 +7,7 @@
     "use strict";
     const state = {
         connectionId: undefined,
+        dirListingMap: new Map(),
         slaveId: undefined,
         socket: new WebSocket(`wss://${location.host}/browserWs/`),
     };
@@ -35,11 +36,27 @@
         console.info(message); // todo : replace with something less obtrusive
     }
 
+    function updateEntry(statData) {
+        const fileName = statData.filePath.replace(/.*\//, "");
+        const entryRow = state.dirListingMap.get(fileName);
+        if (!entryRow) { return; }
+        // octal mask, see http://man7.org/linux/man-pages/man7/inode.7.html
+        const isFile = statData.response.mode >= 32768;
+        // convert to octal representation, and get last three digits
+        const permissions = Number(statData.response.mode).toString(8).replace(/.*(...)/, "$1");
+        const size = statData.response.size;
+        const modTime = new Date(statData.response.mtimeMs);
+        const modTimeString = new Date(statData.response.mtime);
+
+        if (!isFile) { entryRow.iconElement.className = "far fa-folder"; }
+        entryRow.itemDateText = modTimeString;
+        entryRow.itemSizeText = size;
+    }
+
     function listDir(path) {
         fetch(path)
             .then(function(r) { return r.json(); })
             .then(function(responseJson) {
-                console.info("responseJson", responseJson);
                 if (responseJson.listing) {
                     dom.dirlisting.innerHTML = "";
                     responseJson.listing.forEach(function(itemName) {
@@ -54,7 +71,8 @@
 
                         // note: this only works based on file/folder name.
                         // So folder names ending witth valid extensions will be misinterpreted.
-                        itemIconCell.appendChild(makeIcon(itemName));
+                        const itemIconElement = makeIcon(itemName);
+                        itemIconCell.appendChild(itemIconElement);
                         itemNameCell.appendChild(itemNameText);
                         itemSizeCell.appendChild(itemSizeText);
                         itemDateCell.appendChild(itemDateText);
@@ -62,7 +80,15 @@
                         [itemIconCell, itemNameCell, itemSizeCell, itemDateCell]
                             .forEach(function addCell(cell) { itemRow.appendChild(cell); });
 
+                        state.dirListingMap.set(
+                            itemName, {
+                                iconElement: itemIconElement,
+                                itemDateText: itemDateText,
+                                itemSizeText: itemSizeText,
+                                row: itemRow,
+                            });
                         dom.dirlisting.appendChild(itemRow);
+
                         itemRow.addEventListener("click", function() {
                             listDir(path + itemName + "/");
                         });
@@ -70,7 +96,6 @@
                 } else if (responseJson.fileContents) {
                     const a = document.createElement("a");
                     a.href = `data:${responseJson.mimeType};base64,${responseJson.fileContents}`;
-                    console.log(a, responseJson);
                     a.download = responseJson.path.replace(/\/$/, "");
                     document.body.appendChild(a);
                     a.click();
@@ -85,7 +110,6 @@
 
     });
     document.getElementById("clearSlave").addEventListener("click", function() {
-
         state.slaveId = document.getElementById("slaveId").value = "";
         dom.dirlisting.innerHTML = "";
     });
@@ -100,7 +124,9 @@
             if (message.connectionId) {
                 state.connectionId = message.connectionId;
             } else {
-                console.log("other message", event.data);
+                if (event.data.action === "stat") {
+                    updateEntry(event.data);
+                }
             }
         } catch (e) { userFeedback("couldn't parse message from server"); }
     });
